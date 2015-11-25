ed0fb7501ec8cfa242a9f2d3e022b17f84980fa2
iosmichael <michael.liu@my.wheaton.edu>
2015-11-24 23:09:04 -0600

    file update

---
+++ ProxiFinalTests/ProxiFinalTests.m
@@ -0,0 +1,39 @@
+//
+//  ProxiFinalTests.m
+//  ProxiFinalTests
+//
+//  Created by Michael Liu on 10/3/15.
+//  Copyright © 2015 Michael Liu. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ProxiFinalTests : XCTestCase
+
+@end
+
+@implementation ProxiFinalTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
